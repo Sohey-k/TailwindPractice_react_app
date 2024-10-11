061ada9a35fc6bfa87bb2ea9589070e0d5c15d18
team-K <reepoo0528@gmail.com>
2024-10-11 11:23:06 +0900

    Change File

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Menu from "./components/Menu";
+import Sidebar from "./components/Sidebar";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-violet-400">
-      <h1 className="text-6xl font-extrabold">私のポートフォリオ</h1>
-    </div>
-  )
+    <>
+      <Header />
+      <Main />
+      <Menu />
+      <Sidebar />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
