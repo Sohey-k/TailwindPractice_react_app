3ba475ca7d6729799019ea0bbf7f4770af0dde1c
team-K <reepoo0528@gmail.com>
2024-10-14 22:16:03 +0900

    Add Logo.tsx Change code Header.tsx ,Add file to /assets/Rearme.svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
-// import React from 'react'
+import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <div className="pb-2">
-       <h1 className="text-8xl font-serif font-black">RE:ARME</h1>
+    <div className="pb-2 flex items-center">
+      <Logo />
+      <h1 className="text-8xl font-serif font-black">RE:ARME</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,8 @@
+import logo from '../assets/Rearme.svg'
+
 const Logo = () => {
     return(
-
+        <img src={logo} alt="Rearme logo" className='h-24 w-24 rounded-full' />
     )
 }
 
