23a74708f084c9fca63e4744d05f188b8f55a404
team-K <reepoo0528@gmail.com>
2024-11-12 11:43:53 +0900

    Add microCMS on Logs

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -1,9 +1,12 @@
 // src/pages/Logs.tsx
+
+import Articles from '../components/Article';
+
 export const Logs = () => {
     return (
       <div className="p-6">
         <h1 className="font-['Stalinist_One'] text-2xl mb-6">LOGS</h1>
-        {/* ここに自己紹介コンテンツ */}
+         <Articles />
       </div>
     );
   };
