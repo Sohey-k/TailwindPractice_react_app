12b73c9c8255a5826d89152ee50e1163934eac39
team-K <reepoo0528@gmail.com>
2025-12-11 12:26:32 +0900

    Fix image path in Base.tsx

--- src/pages/Base.tsx
+++ src/pages/Base.tsx
@@ -3,7 +3,7 @@ import SlidingText from "../components/SlidingText";
 
 export const Base: React.FC = () => {
   const text = "Welcome to Re:arme\nWeb Site!";
-  const imageUrl = "../../public/Girl-Rearme3.png"; // 画像の実際のパス
+  const imageUrl = "/Girl-Rearme3.png"; // 画像の実際のパス
 
   return (
     <div className="p-[8rem]">
