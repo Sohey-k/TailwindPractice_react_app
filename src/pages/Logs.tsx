8cbb0e6dc6785e34cdb5c1eb2e44247fde37bb46
team-K <reepoo0528@gmail.com>
2025-12-12 16:54:58 +0900

    Modify Logs.tsx

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -6,7 +6,7 @@ export const Logs = () => {
   return (
     <div className="p-6">
       <h1 className="font-bold text-2xl pt-2 pb-2">学習記録</h1>
-      <div className="divider divider-primary"></div>  // 紫色
+      <hr className="border-t-2 border-purple-600 my-4" />
       <Articles />
     </div>
   );
