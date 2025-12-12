a38adc5bf428f4b22e564ac5073005179fd5d8df
team-K <reepoo0528@gmail.com>
2025-12-12 16:58:00 +0900

    Modify Logs.tsx

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -4,7 +4,7 @@ import Articles from "../components/Article";
 
 export const Logs = () => {
   return (
-    <div className="p-6">
+    <div className="pt-8 pb-8 font-zenKaku relative z-0">
       <h1 className="font-bold text-2xl pt-2 pb-2">学習記録</h1>
       <hr className="border-t-2 border-purple-600 my-4" />
       <Articles />
