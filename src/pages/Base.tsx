866405693ba25cfaf196232dad7ddad981789711
team-K <reepoo0528@gmail.com>
2025-12-14 10:08:13 +0900

    Change Base.tsx SVG>PNG

--- src/pages/Base.tsx
+++ src/pages/Base.tsx
@@ -4,7 +4,7 @@ import SlidingText from "../components/SlidingText";
 
 export const Base: React.FC = () => {
   const text = "RE:ARME\nRELOAD - REBUILD - RECODE";
-  const imageUrl = "/Rearme2.svg";
+  const imageUrl = "/Rearme5.png";
 
   return (
     <div className="w-full h-full flex justify-center items-center px-4">
